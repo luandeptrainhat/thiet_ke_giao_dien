448b540abddff3f4a7f01cadced1842032606fae
luandeptrainhat <115625569+luandeptrainhat@users.noreply.github.com>
2022-11-18 13:15:50 +0700

    ấ

--- app/src/main/java/com/example/asm/activity/Thong_ke.java
+++ app/src/main/java/com/example/asm/activity/Thong_ke.java
@@ -12,5 +12,6 @@ public class Thong_ke extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thong_ke);
+
     }
 }
